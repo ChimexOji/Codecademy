d5238bf9f177d2d4fd5ad9b6541055148110159e
Chimezie Oji <chimezieoji@yahoo.com>
2022-12-23 20:51:46 +0100

    Codecademy Capstone React Spotify Query App

---
+++ src/components/App/App.js
@@ -0,0 +1,99 @@
+import React from'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults';
+import Playlist from '../Playlist/Playlist';
+import Spotify from '../../util/Spotify';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchResults: [],
+      playlistName: 'My Playlist',
+      playlistTracks: []
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlayListName = this.updatePlayListName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+
+    // checks if track is in array of saved tracks
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
+  };
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    // if current track id matches saved track id, remove it
+    tracks = tracks.filter(savedTrack => savedTrack.id!== track.id);
+
+    this.setState({playlistTracks: tracks});
+  }
+
+  updatePlayListName(name) {
+    this.setState({ playlistName: name });
+  }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: 'New Playlist',
+        playlistTracks: []
+      });
+    })
+  }
+
+  search(query) {
+    Spotify.search(query).then(searchResults => {
+      this.setState({ searchResults: searchResults })
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar onSearch={this.search} />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} /> 
+            <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} 
+                onRemove={this.removeTrack} onNameChange={this.updatePlayListName} onSave={this.savePlaylist} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
+
+{/* <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div> */}
